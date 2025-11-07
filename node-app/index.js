436ba0df4ab2d55590ed63492d8044dab5b7a2c1
kotyadata <kotyadatejaa@gmail.com>
2025-11-07 09:35:26 +0530

    Update index.js

--- node-app/index.js
+++ node-app/index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('🚀 Hello from Node.js App deployed via Docker!');
+  res.send('Hello from Node.js App deployed');
 });
 
 app.listen(port, () => {
